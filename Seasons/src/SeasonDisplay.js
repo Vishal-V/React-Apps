67002bb428a5a22bfae794a1a495b692d5569f14
Vishal-V <vishal114186@gmail.com>
2019-01-30 20:34:47 +0530

    Season checking logic

--- Seasons/src/SeasonDisplay.js
+++ Seasons/src/SeasonDisplay.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const seasondisplay = {
+const seasonConfig = {
 	winter: {
 		message: "Time to go ice skating!",
 		iconPic: 'snowflake'
@@ -11,9 +11,23 @@ const seasondisplay = {
 	} 
 }
 
+const getSeason = (lat, month) => {
+	if(month > 3 && month < 9)
+		return lat > 0 ? 'summer' : 'winter';
+	else
+		return lat > 0 ? 'winter' : 'summer';
+}
+
 const SeasonDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth());
+	const {message, iconPic} = seasonConfig[season]
+
 	return (
-		<div>Latitude: {props.lat}</div>
+		<div>
+			<i className={`${iconPic} icon`}/>
+			<h1>{message}</h1>
+			<i className={`${iconPic} icon`}/>
+		</div>
 	);
 }
 
