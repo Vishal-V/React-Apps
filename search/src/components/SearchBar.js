f62e827fc79e753678894f92514773ca4b2d755f
Vishal-V <vishal114186@gmail.com>
2019-02-11 21:10:12 +0530

    Custom search styles

--- search/src/components/SearchBar.js
+++ search/src/components/SearchBar.js
@@ -4,7 +4,14 @@ import ReactDOM from 'react-dom';
 class SearchBar extends React.Component {
 	render() {
 		return(
-			<div>Hello!</div>
+			<div className='ui segment'>
+				<form className='ui form'>
+					<div className='field'>
+						<label style={{ fontSize: '1.5em', marginBottom: '12px' }}>Search Bar</label>
+						<input type='text'/>
+					</div>
+				</form>
+			</div>
 		);
 	}
 }
